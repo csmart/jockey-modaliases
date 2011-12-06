07e929901bf6a02c75e4e6a658aff299ed1517e8
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2011-12-06 16:42:50 +0330

    Add the ability to exclude some kmods and to mark kmods without corresponding akmod

--- modalias_generator.sh
+++ modalias_generator.sh
@@ -6,6 +6,9 @@
 #
 
 LATEST_KERNEL_VER="3.1.2-1"
+EXCLUDE_KMOD_PKGS="kmod-xtables-addons"
+NO_AKMOD_PKGS="kmod-staging"
+
 OUTPUT_BASENAME=rpmfusion-modules
 OUTPUT=${OUTPUT_BASENAME}.aliases
 
@@ -21,6 +24,11 @@ echo "================================================================="
 for kmodpkg in *.rpm; do
 	rpmdev-extract "$kmodpkg" > /dev/null
 	PKGNAME="${kmodpkg%%-${LATEST_KERNEL_VER}*}"
+	if [[ $EXCLUDE_KMOD_PKGS == *${PKGNAME}* ]]; then
+		echo "Excluded kmod: $EXCLUDE_KMOD_PKGS"
+		continue;
+	fi
+
 	PKGDIR="${kmodpkg%.rpm}"
 	KMODS="$(find "$PKGDIR" -name "*ko")"
         for kmod in $KMODS; do
@@ -28,7 +36,13 @@ for kmodpkg in *.rpm; do
 		MODULE_NAME=${MODULE_NAME%.ko}
 		modinfo $kmod | grep "alias:.*:" | sed "s|alias: *\(.*\)|alias \1 ${MODULE_NAME} ${PKGNAME}|" >> $OUTPUT
 		modinfo $kmod | grep "alias:.*:" | sed "s|alias: *\(.*\)|alias \1 ${MODULE_NAME} ${PKGNAME}-PAE|" >> $OUTPUT_BASENAME-PAE.aliases
-		modinfo $kmod | grep "alias:.*:" | sed "s|alias: *\(.*\)|alias \1 ${MODULE_NAME} a${PKGNAME}|" >> $OUTPUT_BASENAME-akmods.aliases
+
+		if [[ $NO_AKMOD_PKGS == *${PKGNAME}* ]]; then
+			AKMOD_PKGNAME=$PKGNAME
+		else
+			AKMOD_PKGNAME=a$PKGNAME
+		fi
+		modinfo $kmod | grep "alias:.*:" | sed "s|alias: *\(.*\)|alias \1 ${MODULE_NAME} ${AKMOD_PKGNAME}|" >> $OUTPUT_BASENAME-akmods.aliases
 	done
 	echo "Package: $PKGNAME"
 	rm -rf "${PKGDIR}"
